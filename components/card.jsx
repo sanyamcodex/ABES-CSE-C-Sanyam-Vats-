73d512711be8d23b2e9816b7fcd3f1a182f61ade
Sanyam Vats <166936986+sanyamcodex@users.noreply.github.com>
2026-02-13 12:26:22 +0530

    Add files via upload

---
+++ components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './card.css';
+function Card() {
+  return (
+    <div id='cs'> 
+      <img src='https://www.shutterstock.com/image-vector/vector-cute-baby-panda-cartoon-600nw-2427356853.jpg' alt='' height={100} width={100}/>
+      <h1>titoo singh</h1>
+      <h3>btech</h3>
+    </div>
+  )
+}
+
+export default Card
